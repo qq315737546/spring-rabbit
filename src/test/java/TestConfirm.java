1488acc835025096167407552c9d07d114c294d9
315737546@qq.com <315737546@qq.com>
2017-01-07 19:32:43 +0800

    增加一些注释

--- src/test/java/TestConfirm.java
+++ src/test/java/TestConfirm.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.dingcheng.confirms.publish.PublishService;  
   
-  
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration(locations = {"classpath:application-context.xml"})  
 public class TestConfirm {  
@@ -15,39 +14,39 @@ public class TestConfirm {
     
     private static String exChange = "DIRECT_EX";
       
-//    @Test  
+    @Test  
     public void test1() throws InterruptedException{  
     	String message = "currentTime:"+System.currentTimeMillis();
     	System.out.println("test1---message:"+message);
-    	//exchange,queue 都正确
+    	//exchange,queue 都正确,confirm被回调, ack=true
     	publishService.send(exChange,"CONFIRM_TEST",message);  
-//        Thread.sleep(10000);
+        Thread.sleep(1000);
     }  
     
-//    @Test  
+    @Test  
     public void test2() throws InterruptedException{  
     	String message = "currentTime:"+System.currentTimeMillis();
     	System.out.println("test2---message:"+message);
-    	//exchange 错误,queue 正确
+    	//exchange 错误,queue 正确,confirm被回调, ack=false
     	publishService.send(exChange+"NO","CONFIRM_TEST",message);  
-//        Thread.sleep(10000);
+        Thread.sleep(1000);
     }  
     
     @Test  
     public void test3() throws InterruptedException{  
     	String message = "currentTime:"+System.currentTimeMillis();
     	System.out.println("test3---message:"+message);
-    	//exchange 正确,queue 错误
+    	//exchange 正确,queue 错误 ,confirm被回调, ack=true; return被回调 replyText:NO_ROUTE
     	publishService.send(exChange,"",message);  
-//        Thread.sleep(10000);
+//        Thread.sleep(1000);
     }  
     
-//    @Test  
+    @Test  
     public void test4() throws InterruptedException{  
     	String message = "currentTime:"+System.currentTimeMillis();
     	System.out.println("test4---message:"+message);
-    	//exchange 错误,queue 错误
+    	//exchange 错误,queue 错误,confirm被回调, ack=false
     	publishService.send(exChange+"NO","CONFIRM_TEST",message);  
-//        Thread.sleep(10000);
+        Thread.sleep(1000);
     }  
 }  
